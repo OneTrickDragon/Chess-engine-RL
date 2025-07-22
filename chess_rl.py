a62ff5f6c751773587046bca349d66b7c4ac0b70
OneTrickDragon7 <69195984+OneTrickDragon@users.noreply.github.com>
2025-07-21 22:17:22 -0400

    Changed ENV to always reset board correctly, added draw and win checking

--- chess_rl.py
+++ chess_rl.py
@@ -5,12 +5,12 @@ import chess
 
 class ChessEnv:
     def __init__(self):
-        self.board = chess.Board()
-        self.current_player = 1 #1 = White, -1 = Black
+        self.board = chess.Board(chess.STARTING_FEN)
+        #self.current_player = 1 #1 = White, -1 = Black
 
     def reset(self):
-        self.board = chess.Board()
-        self.current_player = 1
+        self.board = chess.Board(chess.STARTING_FEN)
+        #self.current_player = 1
         return self.get_state()
     
     def get_state(self):
@@ -20,5 +20,17 @@ class ChessEnv:
         return self.board.legal_moves
     
     def step(self, move):
-        action = move
+        self.board.push_san(move)
+
+        if self.board.is_checkmate:
+            if self.board.turn:
+                reward = 1
+                return self.get_state(), reward, True
+            else:
+                reward = -1
+                return self.get_state(), reward, True
         
+        if self.board.is_stalemate or self.board.is_repetition or self.board.is_fifty_moves:
+            reward = 0
+            return self.get_state(), reward, True
+
