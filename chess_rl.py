e9d3762f41abb6b4bc4b831b0b8fb01437cda912
OneTrickDragon7 <69195984+OneTrickDragon@users.noreply.github.com>
2025-11-22 21:05:10 -0500

    Fixed reward logic

--- chess_rl.py
+++ chess_rl.py
@@ -6,15 +6,13 @@ import chess
 class ChessEnv:
     def __init__(self):
         self.board = chess.Board(chess.STARTING_FEN)
-        #self.current_player = 1 #1 = White, -1 = Black
 
     def reset(self):
         self.board = chess.Board(chess.STARTING_FEN)
-        #self.current_player = 1
         return self.get_state()
     
     def get_state(self):
-        return self.board.fen()
+        return self.board
     
     def get_legal_moves(self):
         return self.board.legal_moves
